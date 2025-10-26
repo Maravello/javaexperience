f8c6c363d9518124c0b6187b0f2866ae7545603e
Maravello <yahayac753@gmail.com>
2025-10-26 18:39:11 +0100

    commit

--- my-react-cars/src/Pages/Connexion.jsx
+++ my-react-cars/src/Pages/Connexion.jsx
@@ -5,7 +5,31 @@ function Myconnexion(){
 
 
     return(
-        <Navbar />
+        <div>
+            <Navbar />
+            <form>
+                <table className="TableauFormulaire">
+                <tr>
+                    <th>
+                        Email
+                    </th>
+                    <td>
+                        <input type="email" name="email" id="" />
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Mot de passe
+                    </th>
+                    <td>
+                        <input type="password" name="password" />
+                    </td>
+                </tr>
+                </table>
+            </form>
+        </div>
+        
+            
     )
 }
 
