bfb09b5df0f992fa50c580ac527cd00493ec00e6
Maravello <yahayac753@gmail.com>
2025-10-30 23:49:14 +0100

    commit "j'ai demander a deepseek de me faire un css qui soit responsive"

--- my-react-cars/src/Container/menuAcceuil.jsx
+++ my-react-cars/src/Container/menuAcceuil.jsx
@@ -9,12 +9,12 @@ function MenuAcceuil({isConnected}) {
             <h1>Menu</h1>
             <nav className="TableMenuAcceuil">
                 <ol className="breadcrumb">
-                    {isConnected ?<li className="crumb"><button><Link to="/" state={{isConnected: true}}>Home</Link></button></li> :<li className="crumb"><button><Link to="/" >Home</Link></button></li> }
-                    <li className="crumb"><button><Link to="/apropos">About</Link></button></li>
-                    <li className="crumb"><button><Link to="/Voiture">Voitures</Link></button></li>
-                    <li className="crumb"><button><Link to="/Contact"> Contact</Link></button></li>
-                    {isConnected ?<li className="crumb"><button><Link to="/Connexion" state={{isConnected: true}}> Connexion</Link></button></li> : <li className="crumb"><button><Link to="/Connexion" > Connexion</Link></button></li>}
-                    {isConnected && <li className="crumb"><button><Link to="/Tableau-de-bord">Tableau de bord</Link></button></li>}
+                    {isConnected ?<li className="crumb"><button><Link to="/" style={{textDecoration: "none"}} state={{isConnected: true}}>Home</Link></button></li> :<li className="crumb"><button><Link to="/" >Home</Link></button></li> }
+                    <li className="crumb"><button><Link to="/apropos" style={{textDecoration: "none"}}>About</Link></button></li>
+                    <li className="crumb"><button><Link to="/Voiture" style={{textDecoration: "none"}}>Voitures</Link></button></li>
+                    <li className="crumb"><button><Link to="/Contact" style={{textDecoration: "none"}}> Contact</Link></button></li>
+                    {isConnected ?<li className="crumb"><button><Link to="/Connexion" state={{isConnected: true}} style={{textDecoration: "none"}}> Connexion</Link></button></li> : <li className="crumb"><button><Link to="/Connexion" > Connexion</Link></button></li>}
+                    {isConnected && <li className="crumb"><button><Link to="/Tableau-de-bord" style={{textDecoration: "none"}}>Tableau de bord</Link></button></li>}
                 </ol>
             </nav>
         </div>
